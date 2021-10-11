cf1ee5158ce1ef094eab35435443b7829488f36f
Nichollette <nichollette.aco@gmail.com>
2021-10-11 12:42:26 -0700

    parser doc

--- parser.py
+++ parser.py
@@ -8,8 +8,9 @@ from biothings.utils.dataload import dict_convert, dict_sweep
 logging = config.logger
 
 # Association Parser    
+# Association Parser    
 def load_hmdb_data(data_folder):
-    
+        
     # --- Helper Methods --- 
     # --- Enter subject into document --
     def enter_subject(data, tags):
@@ -66,7 +67,7 @@ def load_hmdb_data(data_folder):
 
 
     # --- Enter mapping IDs into the document --- 
-    def enter_mapping_ids(mapping_dict, text):
+    def enter_mapping_ids(mapping_dict, text, data):
         # get the extra IDs from the metabolite xml
         # {'kegg_id': 'C01092', 'chemspider_id': '4578', 'chebi_id': '127029', 'pubchem_compound_id': '4740'}
         data["object"]["kegg_id"]=mapping_dict[text]["kegg_id"]
@@ -78,6 +79,90 @@ def load_hmdb_data(data_folder):
 
     # ------------------------------------------------- 
 
+    def construct_rec(protein_tree, mapping_dict):
+        records=[]
+        # --- Iterate over the root ---
+        for tags in protein_tree[0].iter():
+            metabolites = [] # holder for metabolite_associations.metabolite, the associations w/o references
+            
+            if tags.tag == "{http://www.hmdb.ca}protein": # get the protein nodes only
+                
+                # we need the first accession number, this is main protein _id in our doc
+                _id = tags.find("{http://www.hmdb.ca}accession")
+                _id = _id.text   
+                protein_type=tags.find("{http://www.hmdb.ca}protein_type")
+                ct=1 # setup counter for the associations
+                
+                
+                # ---------- Metabolite associations with references ------------  
+                for m in tags.findall("{http://www.hmdb.ca}metabolite_references"):
+                    for ref in m:
+                        # set the main accession id 
+                        _id2=_id+"_%s"%ct
+                        ct+=1 # update accession counter
+                        # create dictionary document
+                        data={}
+                        data={ "_id": _id2, "pmid": None, "subject": { "protein_type": protein_type.text}, "object":{}}
+            
+                        # pull out the reference tags and get the pubmed_id
+                        for met_ref in ref.findall("{http://www.hmdb.ca}reference"):
+                            for refs in met_ref:
+                                if "pubmed_id" in refs.tag:
+                                    data['pmid']=refs.text
+
+                        for met in ref.findall("{http://www.hmdb.ca}metabolite"):
+                            for info in met:
+                                tag=info.tag.split("}")[1]
+                                text=info.text
+                                data["object"][tag]=text
+
+                                if "accession" in tag:
+                                    enter_mapping_ids(mapping_dict, text, data)
+                                
+                        # Call enter_subject method to fill in subject data 
+                        data=enter_subject(data,tags)  
+                        #data=dict_convert(data,keyfn=process_key)
+                        #data = dict_sweep(data,vals=[np.nan])
+                        records.append(data)
+
+                # ---------- Metabolite associations without references ------------         
+                # find the metabolite_association tags and extract the information
+                for met_assc in tags.findall("{http://www.hmdb.ca}metabolite_associations"):
+                    for met_assc_ in met_assc.findall("{http://www.hmdb.ca}metabolite"):
+                        for met_assc_id in met_assc_.findall("{http://www.hmdb.ca}accession"):
+                            
+                            # --- Check for duplicate ID, if found, skip making document --- 
+                            # if the metabolite association was already present above (in metabolite_refereces)
+                            # we want to pass adding id to dict to avoid making a duplicate document 
+                            pass_assc=False # set bool 
+                            for elem in data_list:
+                                if met_assc_id.text == elem['object']['accession']:                            
+                                    pass_assc = True
+                                    
+                            # if bool is True pass making duplicate doc       
+                            if pass_assc==True: 
+                                pass
+
+                            # else create the document 
+                            else:
+                                # create data dict for association accession 
+                                data={"_id": _id+"_%s"%ct, 'pmid': 'Unknown', 'subject':{}, 'object':{'accession': met_assc_id.text} }
+                                ct+=1 # update the id counter 
+
+                                enter_mapping_ids(mapping_dict, met_assc_id.text, data) # add the mapping ids for this accession
+
+                                for met_assc_name in met_assc_.findall("{http://www.hmdb.ca}name"):
+                                    data["object"]['name'] = met_assc_name.text
+
+                                # Call enter_subject method to fill in subject data 
+                                data=enter_subject(data,tags)  
+                                #data=dict_convert(data,keyfn=process_key)
+                                #data = dict_sweep(data,vals=[np.nan])
+                                records.append(data)        
+
+        return records;
+
+    # --- Execute Program ---
     process_key = lambda k: k.replace(" ","_").lower()
 
     # --- Set input XML file path ---
@@ -89,87 +174,12 @@ def load_hmdb_data(data_folder):
     protein_tree = ET.XML(xml_data)  # Parse XML
     data_list=[] # final data holder 
     mapping_dict=make_metbolite_dict() # load metabolite file and get the mapping ids 
+    records=construct_rec(protein_tree, mapping_dict)
+    #print(records)
+    
+    if(records):
+        for record in records:
+            yield record #print(json.dumps(record, sort_keys=False, indent=4))
 
 
-    # --- Iterate over the root ---
-    for tags in protein_tree[0].iter():
-        metabolites = [] # holder for metabolite_associations.metabolite, the associations w/o references
-        
-        if tags.tag == "{http://www.hmdb.ca}protein": # get the protein nodes only
-            
-            # we need the first accession number, this is main protein _id in our doc
-            _id = tags.find("{http://www.hmdb.ca}accession")
-            _id = _id.text   
-            protein_type=tags.find("{http://www.hmdb.ca}protein_type")
-            ct=1 # setup counter for the associations
-            
-            
-            # ---------- Metabolite associations with references ------------  
-            for m in tags.findall("{http://www.hmdb.ca}metabolite_references"):
-                for ref in m:
-                    # set the main accession id 
-                    _id2=_id+"_%s"%ct
-                    ct+=1 # update accession counter
-                    # create dictionary document
-                    data={}
-                    data={ "_id": _id2, "pmid": None, "subject": { "protein_type": protein_type.text}, "object":{}}
-        
-                    # pull out the reference tags and get the pubmed_id
-                    for met_ref in ref.findall("{http://www.hmdb.ca}reference"):
-                        for refs in met_ref:
-                            if "pubmed_id" in refs.tag:
-                                data['pmid']=refs.text
-
-                    for met in ref.findall("{http://www.hmdb.ca}metabolite"):
-                        for info in met:
-                            tag=info.tag.split("}")[1]
-                            text=info.text
-                            data["object"][tag]=text
-
-                            if "accession" in tag:
-                                enter_mapping_ids(mapping_dict, text)
-                               
-                    # Call enter_subject method to fill in subject data 
-                    data=enter_subject(data,tags)  
-                    data=dict_convert(data,keyfn=process_key)
-                    data = dict_sweep(data,vals=[np.nan])
-                    data_list.append(data)
-
-            # ---------- Metabolite associations without references ------------         
-            # find the metabolite_association tags and extract the information
-            for met_assc in tags.findall("{http://www.hmdb.ca}metabolite_associations"):
-                for met_assc_ in met_assc.findall("{http://www.hmdb.ca}metabolite"):
-                    for met_assc_id in met_assc_.findall("{http://www.hmdb.ca}accession"):
-                        
-                        # --- Check for duplicate ID, if found, skip making document --- 
-                        # if the metabolite association was already present above (in metabolite_refereces)
-                        # we want to pass adding id to dict to avoid making a duplicate document 
-                        pass_assc=False # set bool 
-                        for elem in data_list:
-                            if met_assc_id.text == elem['object']['accession']:                            
-                                pass_assc = True
-                                
-                        # if bool is True pass making duplicate doc       
-                        if pass_assc==True: 
-                            pass
-
-                        # else create the document 
-                        else:
-                            # create data dict for association accession 
-                            data={"_id": _id+"_%s"%ct, 'pmid': 'Unknown', 'subject':{}, 'object':{'accession': met_assc_id.text} }
-                            ct+=1 # update the id counter 
-
-                            enter_mapping_ids(mapping_dict, met_assc_id.text) # add the mapping ids for this accession
-
-                            for met_assc_name in met_assc_.findall("{http://www.hmdb.ca}name"):
-                                data["object"]['name'] = met_assc_name.text
-
-                            # Call enter_subject method to fill in subject data 
-                            data=enter_subject(data,tags)  
-                            data=dict_convert(data,keyfn=process_key)
-                            data = dict_sweep(data,vals=[np.nan])
-                            data_list.append(data)        
-
-    for doc in data_list:
-        #print(json.dumps(doc_, sort_keys=False, indent=4))
-        yield doc
+    
